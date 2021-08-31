d3c8fcb58a54dc91ea59bea3319c4654412e436a
James <james_kemeny@hotmail.com>
2021-08-31 11:50:31 +1000

    Added object pooling and arrow scripts
    Added james' assets and scene

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PoolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        // Pool class storing its size, tag and prefab
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+
+    public static PoolManager m_Instance;
+
+    private void Awake()
+    {
+        m_Instance = this;
+    }
+
+    #endregion
+
+    public List<Pool> m_Pools;
+    public Dictionary<string, Queue<GameObject>> m_PoolDictionary;
+
+    void Start()
+    {
+        // Create a new dictionary of game objects
+        m_PoolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in m_Pools)
+        {
+            // For each queue we want to make, we create a new queue of objects
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                // Add all the desired objects to the queue
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            m_PoolDictionary.Add(pool.tag, objectPool);
+
+        }
+    }
+
+    public GameObject SpawnFromPool(string _tag, Vector3 _position, Quaternion _rotation)
+    {
+        // Check the pool tag is valid
+        if(!m_PoolDictionary.ContainsKey(_tag))
+        {
+            Debug.LogWarning("Pool with tag " + _tag + " doesn't exist.");
+            return null;
+        }
+
+        GameObject objectToSpawn = m_PoolDictionary[_tag].Dequeue();
+        if(objectToSpawn != null)
+        {
+            objectToSpawn.transform.position = _position;
+            objectToSpawn.transform.rotation = _rotation;
+            objectToSpawn.SetActive(true);
+        }
+
+        IPooledObject pooledObject = objectToSpawn.GetComponent<IPooledObject>();
+
+        if(pooledObject != null)
+        {
+            pooledObject.OnObjectSpawn();
+        }
+
+        m_PoolDictionary[_tag].Enqueue(objectToSpawn);
+        return objectToSpawn;
+    }
+}
---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author: James Kemeny
+
+public class TowerController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        
+    }
+
+    void FixedUpdate()
+    { 
+
+    }
+
+    void Shoot()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+
+    }
+}
